98901163a1da3c408630371e29bf8b898a400e96
unknown <aas.amorim01@gmail.com>
2024-03-08 12:48:44 -0300

    + ajustes, adições e melhorias

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,19 @@
 export const projects = [
   {
     id: 1,
+    name: "Open Music",
+    description:
+    "Projeto de música onde se pode ver os discos por preço ou categoria",
+    stacks: [
+      {id: 1, name: "HTML"},
+      {id: 2, name: "CSS"},
+      {id: 3, name: "Javascript"},
+    ],
+    repo: "https://github.com/Kenzie-Academy-Brasil-Developers/open-music-base-AnddAmorim",
+    deploy: "https://open-music-tau.vercel.app",
+  },
+  {
+    id: 2,
     name: "BurgerKenzie",
     description:
       "Projeto que permite ao usuário manipular um carrinho de compras e pesquisar os produtos disponíveis de uma lanchonete.",
@@ -14,7 +27,7 @@ export const projects = [
     deploy: "https://hamburgueria-andd-amorim.vercel.app",
   },
   {
-    id: 2,
+    id: 3,
     name: "NuKenzie",
     description:
       "Projeto que permite gerenciar transações financeiras, como entrada e saída de lucros e gastos, com o valor final.",
@@ -27,7 +40,7 @@ export const projects = [
     deploy: "https://nu-kenzie-three-gold.vercel.app",
   },
   {
-    id: 3,
+    id: 4,
     name: "KenzieHub",
     description:
       "Projeto que permite cadastrar um usuário e " +
@@ -45,7 +58,7 @@ export const projects = [
     deploy: "https://kenzie-hub-andd-amorim.vercel.app",
   },
   {
-    id: 4,
+    id: 5,
     name: "My Virtual Contacts",
     description:
       "Aplicação FullStack, onde se gerencia contatos, no qual você pode listar, adicionar, atualizar e deletar.",
@@ -64,7 +77,7 @@ export const projects = [
     deploy: "https://my-virtual-contacts.vercel.app",
   },
   {
-    id: 5,
+    id: 6,
     name: "KImóveis",
     description: 
     "Aplicação onde é possível o cadastro de imóveis e de usuários interessados na aquisição de propriedades." +
@@ -79,18 +92,5 @@ export const projects = [
      repo: "https://github.com/Kenzie-Academy-Brasil-Developers/m4-kimoveis-AnddAmorim",
      deploy: "",
   },
-  {
-    id: 6,
-    name: "Kanvas",
-    description:
-    "Aplicação de uma API Rest para o gerenciamento de cursos e aulas de uma escola de modalidade EAD.",
-    stacks: [
-      {id: 1, name: "Python"},
-      {id: 2, name: "Django"},
-      {id: 3, name: "Sqlite3"},
-      {id: 4, name: "DjangoRestFramework"},
-    ],
-    repo: "https://github.com/AnddAmorim/m5-projeto-final-kanvas-AnddAmorim",
-    deploy: "",
-  },
+  
 ];
